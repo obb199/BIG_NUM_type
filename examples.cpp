77799d8334f26405d0a613b929aba6b23f64d053
Gabriel Carvalho Santana <74666057+obb199@users.noreply.github.com>
2021-12-26 19:14:44 -0300

    Update examples.cpp

--- examples.cpp
+++ examples.cpp
@@ -9,14 +9,14 @@ int main(){
 	
 	BIG_NUM::sum(res, big1, big2);
 	
-	res->show_number();
+	res->show_number(); // 3123121231231241 + 3123214
 	cout << "\n\n";
 	
 	BIG_NUM* big3 = new BIG_NUM("9234234123675");
 	BIG_NUM* big4 = new BIG_NUM("6798236464879");
 	BIG_NUM* res2 = new BIG_NUM();
 	
-	BIG_NUM::product(res2, big3, big4);
+	BIG_NUM::product(res2, big3, big4); //9234234123675 * 6798236464879
 	
 	res2->show_number();
 	cout << "\n\n";
@@ -24,7 +24,7 @@ int main(){
 	BIG_NUM* big5 = new BIG_NUM(4096);
 	BIG_NUM* res3 = new BIG_NUM();
 	
-	BIG_NUM::pow(res3, big5, 25);
+	BIG_NUM::pow(res3, big5, 25); //4096 ^ 25
 	res3->show_number();
 	
 	
