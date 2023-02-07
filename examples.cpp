8b50fd92a6a35848e91a6ad8befa576a4b0c930b
Gabriel Carvalho Santana <74666057+obb199@users.noreply.github.com>
2023-02-07 00:03:03 -0300

    Update examples.cpp

--- examples.cpp
+++ examples.cpp
@@ -9,7 +9,7 @@ int main(){
 	
 	BIG_NUM::sum(res, big1, big2);
 	
-	res->show_number(); // 3123121231231241 + 3123214
+	res->show_number(); // 3123121231231241 + 3123214 = 3123121234354455
 	cout << "\n\n";
 	
 	BIG_NUM* big3 = new BIG_NUM("9234234123675");
@@ -18,14 +18,15 @@ int main(){
 	
 	BIG_NUM::product(res2, big3, big4); 
 	
-	res2->show_number(); //9234234123675 * 6798236464879
+	res2->show_number(); //9234234123675 * 6798236464879 = 62776507144797362479910325
 	cout << "\n\n";
 	
 	BIG_NUM* big5 = new BIG_NUM(4096);
 	BIG_NUM* res3 = new BIG_NUM();
 	
 	BIG_NUM::pow(res3, big5, 50); 
-	res3->show_number(); //4096 ^ 50
+	res3->show_number(); //4096 ^ 50 = 
+	//4149515568880992958512407863691161151012446232242436899995657329690652811412908146399707048947103794288197886611300789182395151075411775307886874834113963687061181803401509523685376
 	cout << "\n\n";
 	
 	BIG_NUM* n5 = new BIG_NUM("9523452343076575675");
@@ -33,7 +34,7 @@ int main(){
 	BIG_NUM* res4 = new BIG_NUM();
 
  	BIG_NUM::division(res4, n5, n6);
- 	res4->show_number(); //(int)9523452343076575675/102326597787658575
+ 	res4->show_number(); //(int)9523452343076575675/102326597787658575 = 93
  	cout << "\n\n";
 	
 	return 0;
